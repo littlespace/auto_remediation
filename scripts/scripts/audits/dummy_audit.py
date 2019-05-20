aedbf0c954113e44897bec0fe687a05659328f60
Mayuresh Gaitonde <mgaitonde@sm-swang1.roblox.local>
2019-05-20 15:50:46 -0700

    add dummy scripts back

---
+++ scripts/scripts/audits/dummy_audit.py
@@ -0,0 +1,46 @@
+import logging
+import time
+import json
+import random
+import sys
+
+
+class DummyAudit:
+
+    def __init__(self, logger, opts):
+        self.logger = logger
+        self.opts = opts
+
+    def run(self, inp, args):
+        self.logger.info('Got incident Name: {}, Id: {}'.format(
+            inp['name'], inp['id']))
+        passed = True
+        message = 'Audit passed'
+        data = inp['data']
+        if inp['is_aggregate']:
+            components = data.get('components', [])
+            self.logger.info(
+                'Incident has {} components'.format(len(components)))
+        else:
+            components = [data]
+        ents = []
+        for c in components:
+            ent = '{}:{}'.format(c.get('device'), c['entity'])
+            self.logger.info('Running audit for {}'.format(ent))
+            ents.append(ent)
+            time.sleep(10)
+        r = random.randint(0, 100)
+        if r > int(args.pass_percent):
+            passed = False
+            message = 'Audit Failed'
+        self.logger.info(message)
+        out = {
+            'audit': 'dummy_audit',
+            'entities': ents,
+            'passed': passed,
+            'message': message
+        }
+        print(json.dumps(out))
+        if not passed:
+            sys.exit(1)
+        sys.exit(0)
---
+++ scripts/scripts/remediations/dummy_remediation.py
@@ -0,0 +1,38 @@
+import logging
+import time
+import json
+import sys
+
+
+class DummyRemediation:
+
+    def __init__(self, logger, opts):
+        self.logger = logger
+        self.opts = opts
+
+    def run(self, inp, args):
+        self.logger.info('Got incident Name: {}, Id: {}'.format(
+            inp['name'], inp['id']))
+        t = time.time()
+        data = inp['data']
+        if inp['is_aggregate']:
+            components = data.get('components', [])
+            self.logger.info(
+                'Incident has {} components'.format(len(components)))
+        else:
+            components = [data]
+        ents = []
+        for c in components:
+            ent = '{}:{}'.format(c.get('device'), c['entity'])
+            self.logger.info('Running remediation for {}'.format(ent))
+            ents.append(ent)
+            time.sleep(5)
+        self.logger.info('Done')
+        out = {
+            'remediation': 'dummy_remediation',
+            'entities': ents,
+            'elapsed_time': int(time.time() - t),
+            'message': 'Remediation successful',
+        }
+        print(json.dumps(out))
+        sys.exit(0)
